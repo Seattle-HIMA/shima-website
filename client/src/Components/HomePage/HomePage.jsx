6a5b2febe68b3a9180a9c818de3db18029887cf7
Shiina Sugioka <shiina12@uw.edu>
2024-02-03 18:15:13 -0800

    title section done

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -2,16 +2,29 @@ import React, {useState} from 'react';
 
 import './HomePage.css';
 
+import seattleImg from '../../utils/images/space-needle-sunset.png';
+
+const learnMoreButton = () => {
+    console.log("you clicked the learn more button");
+};
+
 function HomePage() {
     return (
-        <div className={"title-section"}>
-            <p className={"title-text"}>
-                Seattle Health Information Management Association
-            </p>
-            <p className={"description-text"}>
-                Welcome! We are a dedicated group of volunteers working to ensure that the Seattle HIM community has
-                access to a network of professionals, engaging workshops and scholarships.
-            </p>
+        <div>
+            <div className={"title-section"}>
+                <p className={"title-text"}>
+                    Seattle Health Information Management Association
+                </p>
+                <p className={"description-text"}>
+                    Welcome! We are a dedicated group of volunteers working to ensure that the Seattle Health
+                    Information Management community has access to a network of professionals, engaging workshops and
+                    scholarships.
+                </p>
+                <button className={"learn-more-button"} onClick={learnMoreButton}>Learn More</button>
+            </div>
+            <div className={"title-image"}>
+                <img src={seattleImg} alt={"space needle sunset"}/>
+            </div>
         </div>
     );
 }
