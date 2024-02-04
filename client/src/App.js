cf81ec5d0bec65327d33017da86ecf4aa926e8cb
Shiina Sugioka <shiina12@uw.edu>
2024-02-03 17:28:08 -0800

    added title and description text

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-
+import HomePage from './Components/HomePage/HomePage'
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
   return(
-    <div>Hi</div>
+    <HomePage />
   )
 }
 
--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,8 +1,18 @@
 import React, {useState} from 'react';
 
+import './HomePage.css';
+
 function HomePage() {
     return (
-        <div>Hi</div>
+        <div className={"title-section"}>
+            <p className={"title-text"}>
+                Seattle Health Information Management Association
+            </p>
+            <p className={"description-text"}>
+                Welcome! We are a dedicated group of volunteers working to ensure that the Seattle HIM community has
+                access to a network of professionals, engaging workshops and scholarships.
+            </p>
+        </div>
     );
 }
 
