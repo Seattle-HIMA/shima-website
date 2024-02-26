50347c4c71e6fd49cced1d34a442d6ce7cc5b13d
Allison-Ho <qnho@uw.edu>
2024-02-25 23:39:20 -0800

    finished what we do section

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -8,7 +8,22 @@ const learnMoreButton = () => {
     console.log("you clicked the learn more button");
 };
 
+// will move into a json file later
+const WHATWEDO = {
+    "Opportunities": `Host workshops, panel disscussions, speakers, and
+                        create networking opportunities.`,
+    "Scholarships": `Provide Undergraduate and Graduate Scholarships for Health
+                        Information Management and Informatics students.`,
+    "Reimbursement": `Provide professional exam fee reimbursement for the AHIMA
+                        credentials.`
+}
+
 function HomePage() {
+    const whatWeDoCards = Object.keys(WHATWEDO).map((key) => {
+        const card = makeCard(key, WHATWEDO[key]);
+        return card;
+    });
+
     return (
         <div>
             <div className={"title-section"}>
@@ -25,8 +40,31 @@ function HomePage() {
             <div className={"title-image"}>
                 <img src={seattleImg} alt={"space needle sunset"}/>
             </div>
+            <div className={"what-we-do-section"}>
+                <p className={"what-we-do-text"}>
+                    What we do
+                </p>
+                <div className={"what-we-do-cards"}>
+                    {whatWeDoCards}
+                </div>
+            </div>
         </div>
     );
+}
+
+function makeCard(name, content) {
+    return (
+        <div className={"what-we-do-card"}>
+            <div className={"card-header"}>
+                <p className={"img"}>insert image</p>
+            </div>
+
+            <div className={"card-body"}>
+                <h2 className={"card-name"}>{name}</h2>
+                <p className={"card-content"}>{content}</p>
+            </div>
+        </div>
+    )
 }
 
 export default HomePage;
