72b1c6e1abda00cb1844381e42331a78de3f7974
shiinasugioka <91639423+shiinasugioka@users.noreply.github.com>
2024-02-27 14:17:13 -0800

    Revert "scholarships header"

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {Route, Routes, Link} from 'react-router-dom';
 import NavBar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
 import Membership from './Components/MembershipPage/MembershipPage';
-import Scholarship from './Components/ScholarshipsPage/ScholarshipsPage';
+import ResourcesPage from './Components/ResourcesPage/ResourcesPage';
 import EventsPage from './Components/EventsPage/EventsPage';
 import AboutUs from './Components/AboutUs/AboutUs';
 import Footer from './Components/Footer/Footer';
@@ -13,25 +13,25 @@ import './utils/variables.css';
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
-  return(
-    <div>
-      <header>
-        <NavBar></NavBar>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<HomePage />}></Route>
-          <Route path='/Membership' element={<Membership />}></Route>
-          <Route path='/ScholarShips' element={<Scholarship />}></Route>
-          <Route path='/Events' element={<EventsPage />}></Route>
-          <Route path='/About' element={<AboutUs />}></Route>
-        </Routes>
-      </main>
-      <footer>
-        <Footer></Footer>
-      </footer>
-    </div>
-  )
+    return (
+        <div>
+            <header>
+                <NavBar></NavBar>
+            </header>
+            <main>
+                <Routes>
+                    <Route index element={<HomePage/>}></Route>
+                    <Route path="/Membership" element={<Membership/>}></Route>
+                    <Route path="/Scholarships" element={<ResourcesPage/>}></Route>
+                    <Route path="/Events" element={<EventsPage/>}></Route>
+                    <Route path="/About" element={<AboutUs/>}></Route>
+                </Routes>
+            </main>
+            <footer>
+                <Footer></Footer>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/Components/ResourcesPage/ResourcesPage.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function ResourcesPage() {
+  return(
+    <div>Hi hi</div>
+  )
+}
+
+export default ResourcesPage;
