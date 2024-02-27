a993b22fe78e05c03092747f2f1fc36347bed936
Shiina Sugioka <shiina12@uw.edu>
2024-02-26 16:21:50 -0800

    benefits card

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -22,7 +22,7 @@ const getTitleSection = () => {
             <button className={"about-us-button"} onClick={aboutUsButton}>About Us</button>
         </div>
     );
-}
+};
 
 const getBecomeAMemberSection = () => {
     return (
@@ -32,11 +32,57 @@ const getBecomeAMemberSection = () => {
             <button className={"membership-button"}>Membership</button>
         </div>
     );
-}
+};
 
 const getMembershipCards = () => {
-
-}
+    return (
+        <div className={"membership-cards-container"}>
+            <div className={"membership-card"}>
+                <div className={"membership-card-left"}>
+                    <p className={"membership-card-title"}>Benefits</p>
+                    <p className={"membership-card-subtitle"}>What we offer.</p>
+                </div>
+                <div>
+                    <div className={"membership-card-content membership-benefits-card-content"}>
+                        <ul className={"benefits-list"}>
+                            <li className={"benefits-list-item"}>Scholarships</li>
+                            <li className={"benefits-list-item"}>
+                                Inexpensive Continuing Education Units (CEUs) to maintain your professional
+                                certification
+                            </li>
+                            <li className={"benefits-list-item"}>Low membership fees</li>
+                        </ul>
+                        <ul className={"benefits-list"}>
+                            <li className={"benefits-list-item"}>Networking</li>
+                            <li className={"benefits-list-item"}>Professional and personal development</li>
+                            <li className={"benefits-list-item"}>Stay up-to-date on information management and
+                                informatics
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={"membership-card"}>
+                <div className={"membership-card-left"}>
+                    <p className={"membership-card-title"}>Eligibility</p>
+                    <p className={"membership-card-subtitle"}>Who should join.</p>
+                </div>
+                <div>
+                    <div className={"membership-card-content membership-eligibility-card-content"}>
+                        <ul>
+                            Current and future professionals interested in the intersection between people, data and
+                            technology
+                        </ul>
+                        <ul>
+                            Professionals working formally or informally in a Health Information Management (HIM) or
+                            healthcare informatics role
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 // will move into a json file later
 const WHATWEDO = {
@@ -65,8 +111,9 @@ function HomePage() {
             <div className={"become-a-member-section"}>
                 {getBecomeAMemberSection()}
             </div>
-            <div className={"membership-cards"}>
+            <div className={"membership-cards-section"}>
                 {getMembershipCards()}
+                <div className={"membership-section-background"}></div>
             </div>
             <div className={"what-we-do-section"}>
                 <p className={"what-we-do-text"}>
