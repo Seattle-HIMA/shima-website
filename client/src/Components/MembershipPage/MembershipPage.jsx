766c84f31ac42ed267ee46e3170ae9354d17c075
annie-tu <annietu18@gmail.com>
2024-02-27 17:12:26 -0800

    add pricing boxes

--- client/src/Components/MembershipPage/MembershipPage.jsx
+++ client/src/Components/MembershipPage/MembershipPage.jsx
@@ -21,17 +21,31 @@ function MembershipPage() {
       <div class="information-section"> 
         <div>
           <h2>Why join?</h2>
+          <p class="caption">Here at SHIMA, we offer discounted workshops, professional development, networking opportunities, Inexpensive Continuing Education Units (CEUs), and volunteering to members.</p>
+          <div id="annual-rates">
+            <h3>Annual Rates</h3>
+            <div class="rates">
+              <div class="price-box">
+                <p class="price-text"><span class="price">$5</span> / year</p>
+                <p class="price-type">Student</p>
+              </div>
+              <div class="price-box">
+                <p class="price-text"><span class="price">$20</span> / year</p>
+                <p class="price-type">Professional</p>
+              </div>
+            </div>
+          </div>
         </div>
         <br></br>
         <div>
           <h2>How to apply</h2>
-          <h3>Online</h3>
+          <h4>Online</h4>
           <p>
             Complete the 2024 SHIMA membership form.
             Once you have completed the membership form you will receive a confirmation email with a link to PayPal.
           </p>
           <button class="apply-button-2">Apply now</button>
-          <h3>In-person</h3>
+          <h4>In-person</h4>
           <p>
             Attend one of our upcoming workshops and complete a new membership form.
           </p>
