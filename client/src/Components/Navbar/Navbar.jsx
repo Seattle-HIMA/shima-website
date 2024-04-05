bda77a6e9fa4385437719db0a33e36984c1427f3
Shiina Sugioka <shiina12@uw.edu>
2024-04-04 18:45:18 -0700

    updated homepage header to match figma

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 function NavBar() {
-  return (
-    <div className={"navbar"}>
-      <span className={"logo"}>Logo</span>
-      <div className={"pages-links"}>
-        <Link className={"nav-btn"} to='/'>Home</Link>
-        <Link className={"nav-btn"} to='/Membership'>Membership</Link>
-        <Link className={"nav-btn"} to='/Scholarships'>Scholarships</Link>
-        <Link className={"nav-btn"} to='/Events'>Events</Link>
-        <Link className={"nav-btn"} to='/About'>About Us</Link>
-      </div>
-      <div className={"account-btns"}>
-        <button>Login</button>
-        <button>Sign up</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={"navbar"}>
+            <span className={"logo"}>Logo</span>
+            <div className={"pages-links"}>
+                <Link className={"nav-btn"} to='/'>Home</Link>
+                <Link className={"nav-btn"} to='/Membership'>Membership</Link>
+                <Link className={"nav-btn"} to='/Scholarships'>Scholarships</Link>
+                <Link className={"nav-btn"} to='/Events'>Events</Link>
+                <Link className={"nav-btn"} to='/About'>About Us</Link>
+            </div>
+            <div className={"account-btns"}>
+                <button>Login</button>
+                <button>Sign up</button>
+            </div>
+        </div>
+    );
 }
 
 export default NavBar;
