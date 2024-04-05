90c3943fefa85aa73309f0fac80e43882e06a04e
Shiina Sugioka <shiina12@uw.edu>
2024-04-05 16:10:13 -0700

    started redesign for the log in

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -6,32 +6,34 @@ import cityImg from '../../utils/images/undraw_city_life_gnpr.svg';
 function Login() {
     return (
         <div className={"login-page-wrapper"}>
-            <div className={"login-title"}>
-                <div className={"login-title-text"}>Seattle Health Information Management Association</div>
-                <img src={cityImg} alt={"walking in the city"} className={"cityImg"}/>
-            </div>
-            <div className={"login-box"}>
-                <div className={"login-box-text"}>Log In</div>
-
-                <div className={"login-email-wrapper"}>
-                    <p className={"login-email-label"}>Email</p>
-                    <input className={"login-email-input"}/>
+            <div className={"login-page-top-half"}>
+                <div className={"login-title"}>
+                    <img src={cityImg} alt={"walking in the city"} className={"cityImg"}/>
+                    <div className={"login-title-text"}>Seattle Health Information <br /> Management Association</div>
                 </div>
-                <div className={"login-password-wrapper"}>
-                    <p className={"login-password-label"}>Password</p>
-                    <input className={"login-password-input"}/>
+                <div className={"login-box"}>
+                    <div className={"login-box-text"}>Log In</div>
+
+                    <div className={"login-email-wrapper"}>
+                        <p className={"login-email-label"}>Email</p>
+                        <input className={"login-email-input"}/>
+                    </div>
+                    <div className={"login-password-wrapper"}>
+                        <p className={"login-password-label"}>Password</p>
+                        <input className={"login-password-input"}/>
+                    </div>
+
+                    <p className={"dont-have-account"}>Don't have an account?
+                        <a className={"create-account-link"}>Create an account</a>
+                    </p>
+
+                    <button className={"login-submit-button"}>
+                        <span>Log In</span>
+                        <span></span>
+                    </button>
                 </div>
-
-                <p className={"dont-have-account"}>Don't have an account?
-                    <a className={"create-account-link"}>Create an account</a>
-                </p>
-
-                <button className={"login-submit-button"}>
-                    <span>Log In</span>
-                    <span></span>
-                </button>
-
             </div>
+            <hr className={"login-bottom-line"}/>
         </div>
     )
 }
