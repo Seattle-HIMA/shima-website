265179ffbf9731c8c384dbbb2d603325b9f8878f
annie-tu <annietu18@gmail.com>
2024-04-09 21:48:03 -0700

    alternate event styles

--- client/src/Components/EventsPage/EventsPage.jsx
+++ client/src/Components/EventsPage/EventsPage.jsx
@@ -8,7 +8,7 @@ const EVENT_INFO = [
     "title": "Successful Healthcare IT projects",
     "speaker": "Tabitha Lieberman",
     "description": "A renowned health IT leader, Tabitha Lieberman has more than 30 years of experience powering transformational implementations, digital integrations, and deployments.",
-    "flyerSource": "flyer-1.png"
+    "flyerSource": "flyer-1.png",
   }, 
   {
     "title": "From Data Entry to Policy Input",
@@ -20,32 +20,40 @@ const EVENT_INFO = [
     "title": "Event 3",
     "speaker": "Speaker 3",
     "description": "description here",
-    "flyerSource": "flyer-1.png"
+    "flyerSource": "flyer-3.jpg"
   }
 ]
 
-function makeSection(title, speaker, description, flyer) {
+function makeSection(title, speaker, description, flyer, index) {
+  console.log("index:", index)
   const flyerImg = require(`../../utils/images/${flyer}`);
+  const styleNum = index % 2 === 0 ? 1 : 2;
+  console.log(styleNum)
   return (
-    <div className={"event-section"}>
-      <div className={"section-image"}>
+    <div className={"event-section" + styleNum}>
+      <div className={"section-image" + styleNum}>
         <img src={flyerImg} alt="Event Flyer" />
       </div>
-      <div className={"section-body"}>
-        <h2 className={"section-title"}>
+      <div className={"section-body"+ styleNum}>
+        <h2 className={"section-title"+ styleNum}>
           {title}<br></br><span>by {speaker}</span>
         </h2>
-        <p className={"section-description"}>{description}</p>
-        <button>Click here</button>
+        <p className={"section-description" + styleNum}>{description}</p>
+        <button class={"event-button" + styleNum}>Click here</button>
       </div>
     </div>
   );
+  /* annie's notes
+  if styleNum is 1, return div with image first and .1 styles
+  if styleNum is 2, return div with text elements above image
+  may be easier to align the image to the right this way and prob makes more sense
+  */
 
 }
 
 function EventsPage() {
-  const eventSections= EVENT_INFO.map((item) => {
-      const section = makeSection(item.title, item.speaker, item.description, item.flyerSource);
+  const eventSections= EVENT_INFO.map((item, index) => {
+      const section = makeSection(item.title, item.speaker, item.description, item.flyerSource, index);
       return section;
   });
 
@@ -60,13 +68,16 @@ function EventsPage() {
           </section>
         </div>
       </div>
-      <div class="events-header">
+      <div class="events-label">
         <h2>Upcoming workshops</h2>
         <p>Registration Form</p>
         <p>Speaker Interest Form</p>
       </div>
       <div>
         {eventSections}
+      </div>
+      <div class="events-label">
+        <h2>Previous Workshops</h2>
       </div>
     </div>
   )
