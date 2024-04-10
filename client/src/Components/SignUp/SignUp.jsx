e98bb5dc26e3eb9152903e8fcc1ba04f892de2e3
Shiina Sugioka <shiina12@uw.edu>
2024-04-10 15:14:37 -0700

    finished sing-up front-end

--- client/src/Components/SignUp/SignUp.jsx
+++ client/src/Components/SignUp/SignUp.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 
 import './SignUp.css';
 import cityImg from '../../utils/images/undraw_city_life_gnpr.svg';
+import googleLogo from "../../utils/icons/google_signin_light.svg";
+import microsoftLogo from "../../utils/icons/ms-symbollockup_signin_light.svg";
 
-function SignUp() {
+function SignUp(props) {
+    props.setShowFooter(false);
+
     return (
         <div className={"signup-page-wrapper"}>
             <div className={"signup-title"}>
-                <div className={"signup-title-text"}>Seattle Health Information Management Association</div>
                 <img src={cityImg} alt={"walking in the city"} className={"cityImg"}/>
+                <div className={"signup-title-text"}>Seattle Health Information <br/> Management Association</div>
             </div>
-            <div className={"signup-box"}>
-                <div className={"signup-box-text"}>Create an account</div>
 
-                <div className={"signup-name-wrapper"}>
-                    <p className={"signup-name-label"}>Full Name</p>
-                    <input className={"signup-name-input"}/>
+            <div className={"signup-box"}>
+                <div className={"signup-box-text"}>Sign Up</div>
+
+                <div className={"signup-fullname-wrapper"}>
+                    <p className={"signup-fullname-label"}>Full Name</p>
+                    <input className={"signup-fullname-input"}/>
                 </div>
                 <div className={"signup-email-wrapper"}>
                     <p className={"signup-email-label"}>Email</p>
@@ -26,14 +31,44 @@ function SignUp() {
                     <input className={"signup-password-input"}/>
                 </div>
 
-                <p className={"already-have-account"}>Already have an account?
-                    <a className={"log-into-account-link"} href={'/LogIn'}>Log In</a>
-                </p>
-
                 <button className={"signup-submit-button"}>
-                    <span>Sign Up</span>
+                    <span>Create an Account</span>
                     <span></span>
                 </button>
+
+                <div className={"signup-or-section"}>
+                    <hr className={"signup-or-line"}/>
+                    <p className={"signup-or-text"}>or</p>
+                </div>
+
+                <div className={"signup-with-google"}>
+                    <button className={"gsi-material-button"}>
+                        <div className={"gsi-material-button-state"}></div>
+                        <div className={"gsi-material-button-content-wrapper"}>
+                            <div className={"gsi-material-button-icon"}>
+                                <img src={googleLogo} alt={"google logo"}/>
+                            </div>
+                            <span className={"gsi-material-button-contents"}>Sign up with Google</span>
+                        </div>
+                    </button>
+                </div>
+
+                <div className={"signup-with-microsoft"}>
+                    <button className={"gsi-material-button"}>
+                        <div className={"gsi-material-button-state"}></div>
+                        <div className={"gsi-material-button-content-wrapper"}>
+                            <div className={"gsi-material-button-icon"}>
+                                <img className={"signup-microsoft-logo"} src={microsoftLogo} alt={"msft logo"}/>
+                            </div>
+                            <span className={"msft-button-contents"}>Sign up with Microsoft</span>
+                        </div>
+                    </button>
+                </div>
+
+                <p className={"already-have-account"}>Already have an account?
+                    <a className={"sign-into-account-link"}>Log In</a>
+                </p>
+
             </div>
         </div>
     )
