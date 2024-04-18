9b099cb537e6dc077b371b0db8ce4d47650a680a
Allison-Ho <qnho@uw.edu>
2024-04-18 08:47:40 -0700

    update the updateJSONFile function to work w new structure

--- server/routes/controls/homepage.js
+++ server/routes/controls/homepage.js
@@ -18,27 +18,29 @@ router.get('/', async (req, res) => {
   }
 });
 
-// update homepage information
-router.post('/update', async(req, res) => {
-  res.type('text');
+router.post('/update', async (req, res) => {
   try{
-    // get body params
-    let {section, info} = req.body;
-    if(section && info){
-      let result = ""
-      let data = await getJSONFile('./assets/homepage.json');
-      // update json file according to given information
-      if(section === "homepage") {
-        result = await updateJSONFile('./assets/homepage.json', {"title": info.updatedPart, "value": info.updatedInfo})
-      } else if (data[section]) {
-        result = await updateJSONFile('./assets/homepage.json', {"title": section[info.updatedPart], "value": info.updatedInfo})
+    // get body params from frontend
+    // format:
+    //    section: the name of the modified section
+    //    newInfo = { partToChange: 'new value' }
+    let { section, newInfo } = req.body;
+    if (section && newInfo) {
+      updateData = {}
+      if (section === 'homepage') {
+        updateData.section = section;
+        updateData.part = newInfo.partToChange;
+        updateData.updatedInfo = newInfo.value;
+      } else if (data.subsections.section) {
+        let subsection = data.subsections;
+        updateData.subsection.section = section;
+        updateData.subsection.part = newInfo.partToChange;
+        updateData.subsection.updatedInfo = newInfo.value;
       } else {
         res.type('text').status(400).send(`There's no ${section} section on this page`);
       }
-
-      res.send(result);
-    }else {
-      res.status(400).send("Updated section or updated information is missing.");
+    } else {
+      res.status(400).type('text').send("Updated section or updated information is missing.");
     }
   }catch(err){
     if (err.code === "ENOENT") {
