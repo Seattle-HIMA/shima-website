2173b7eecb90be8e61b288aa651f90a53cb2c024
Shiina Sugioka <shiina12@uw.edu>
2024-04-18 15:25:37 -0700

    added effect hook to profile

--- client/src/Components/ProfilePage/ProfilePage.jsx
+++ client/src/Components/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "../Auth/LogoutButton";
 
 import './ProfilePage.css'
 
-function ProfilePage() {
+function ProfilePage(props) {
+    props.setShowFooter(true);
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     const {user, isLoading, isAuthenticated} = useAuth0();
 
     if (isLoading) {
