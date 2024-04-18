dd0fc51c823a038fe23aa5b6b0320e2c24eb2641
Shiina Sugioka <shiina12@uw.edu>
2024-04-18 15:51:08 -0700

    updated classnames to something non generic

--- client/src/Components/MembershipPage/MembershipPage.jsx
+++ client/src/Components/MembershipPage/MembershipPage.jsx
@@ -1,8 +1,7 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './MembershipPage.css';
 
 import backgroundImg from '../../utils/images/membership-background.png';
-import qrImg from '../../utils/images/QR-code.png';
 
 function MembershipPage(props) {
     props.setShowFooter(true);
@@ -12,58 +11,61 @@ function MembershipPage(props) {
 
     return (
         <div>
-            <div class="membership-title">
+            <div className="membership-title">
                 <img src={backgroundImg} alt="Seattle skyline" id="membership-img"></img>
-                <div class="centered">
-                    <h1 class="membership-h1">Membership</h1>
+                <div className="membership-title-centered">
+                    <h1 className="membership-h1">Membership</h1>
                     <p className={"caption-text"}>
-                        SHIMA Membership is annual. If you are unsure if you are currently a member, please feel free to
-                        email us.
+                        SHIMA Membership is annual. If you are unsure if you are currently a member,
+                        please feel free to email us.
                     </p>
                 </div>
             </div>
             <br></br>
-            <div class="information-section">
+            <div className="information-section">
                 <div>
-                    <h2 class="membership-h2">Why join?</h2>
-                    <p class="caption">Here at SHIMA, we offer discounted workshops, professional development,
+                    <h2 className="membership-why-join-text">Why join?</h2>
+                    <p className="membership-why-join-description">
+                        Here at SHIMA, we offer discounted workshops, professional development,
                         networking opportunities, Inexpensive Continuing Education Units (CEUs), and volunteering to
-                        members.</p>
-                    <div id="annual-rates">
-                        <h3 class="membership-h3">Annual Rates</h3>
-                        <div class="rates">
-                            <div class="price-box">
-                                <p class="price-text"><span class="price">$5</span> / year</p>
-                                <p class="price-type">Student</p>
+                        members.
+                    </p>
+                    <div className="membership-annual-rates-section">
+                        <h3 className="membership-annual-rates-text">Annual Rates</h3>
+                        <div className="membership-page-rates-cards">
+                            <div className="membership-page-price-box">
+                                <p className="membership-page-price-text"><span className="membership-page-price-num">$5</span> / year</p>
+                                <p className="membership-page-price-type">Student</p>
                             </div>
-                            <div class="price-box">
-                                <p class="price-text"><span class="price">$20</span> / year</p>
-                                <p class="price-type">Professional</p>
+                            <div className="membership-page-price-box">
+                                <p className="membership-page-price-text"><span className="membership-page-price-num">$20</span> / year</p>
+                                <p className="membership-page-price-type">Professional</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <br></br>
-                <div class="apply-section">
-                    <div class="apply-text">
-                        <h2 class="membership-h2">How to apply</h2>
+                <div className="membership-page-apply-section">
+                    <div className="membership-page-apply-section-text">
+                        <h2 className="membership-h2">How to apply</h2>
                         <h4>Online</h4>
-                        <p class="caption">
-                            Complete the 2024 SHIMA membership form below <span class="bold">OR</span> by scanning the
+                        <p className="membership-page-apply-section-subtext">
+                            Complete the 2024 SHIMA membership form below <span>OR</span> by scanning
+                            the
                             QR code.
                             Once you have completed the membership form you will receive a confirmation email with a
                             link to PayPal.
                         </p>
-                        <button class="apply-button">Apply</button>
+                        <button className="membership-page-apply-button">Apply</button>
                         <h4>In-person</h4>
-                        <p class="caption">
+                        <p className="membership-page-apply-section-subtext">
                             Attend one of our upcoming workshops and complete a new membership form.
                         </p>
                     </div>
-                </div> 
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default MembershipPage;
