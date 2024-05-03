7563eeff6186dd6b7fde8c658446f573fe265115
annie-tu <annietu18@gmail.com>
2024-05-03 16:33:57 -0700

    desktop membership page

--- client/src/Components/MembershipPage/MembershipPage.jsx
+++ client/src/Components/MembershipPage/MembershipPage.jsx
@@ -11,7 +11,7 @@ function MembershipPage(props) {
     }, [])
 
     return (
-        <div className="full-width">
+        <div>
             <div class="membership-page">
                 <div class="header">
                     <img src={backgroundImg} alt="Seattle skyline"></img>
@@ -23,46 +23,47 @@ function MembershipPage(props) {
                         </p>
                     </section>
                 </div>
-            </div>
-            <br></br>
-            <div class="information-section">
-                <div>
-                    <h2 class="membership-h2">Why join?</h2>
-                    <p class="caption">Here at SHIMA, we offer discounted workshops, professional development,
-                        networking opportunities, Inexpensive Continuing Education Units (CEUs), and volunteering to
-                        members.</p>
-                    <div id="annual-rates">
-                        <h3 class="membership-h3">Annual Rates</h3>
-                        <div class="rates">
-                            <div class="price-box">
-                                <p class="price-text"><span class="price">$5</span> / year</p>
-                                <p class="price-type">Student</p>
-                            </div>
-                            <div class="price-box">
-                                <p class="price-text"><span class="price">$20</span> / year</p>
-                                <p class="price-type">Professional</p>
+        
+                <br></br>
+                <div class="information-section">
+                    <div>
+                        <h2>Why join?</h2>
+                        <p class="caption">Here at SHIMA, we offer discounted workshops, professional development,
+                            networking opportunities, Inexpensive Continuing Education Units (CEUs), and volunteering to
+                            members.</p>
+                        <div id="annual-rates">
+                            <h3 class="membership-h3">Annual Rates</h3>
+                            <div class="rates">
+                                <div class="price-box">
+                                    <p class="price-text"><span class="price">$5</span> / year</p>
+                                    <p class="price-type">Student</p>
+                                </div>
+                                <div class="price-box">
+                                    <p class="price-text"><span class="price">$20</span> / year</p>
+                                    <p class="price-type">Professional</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <br></br>
+                    <div class="apply-section">
+                        <div class="apply-text">
+                            <h2>How to apply</h2>
+                            <h4>Online</h4>
+                            <p class="caption">
+                                Complete the 2024 SHIMA membership form below <span class="bold">OR</span> by scanning the
+                                QR code.
+                                Once you have completed the membership form you will receive a confirmation email with a
+                                link to PayPal.
+                            </p>
+                            <button class="apply-button">Apply</button>
+                            <h4>In-person</h4>
+                            <p class="caption">
+                                Attend one of our upcoming workshops and complete a new membership form.
+                            </p>
+                        </div>
+                    </div> 
                 </div>
-                <br></br>
-                <div class="apply-section">
-                    <div class="apply-text">
-                        <h2 class="membership-h2">How to apply</h2>
-                        <h4>Online</h4>
-                        <p class="caption">
-                            Complete the 2024 SHIMA membership form below <span class="bold">OR</span> by scanning the
-                            QR code.
-                            Once you have completed the membership form you will receive a confirmation email with a
-                            link to PayPal.
-                        </p>
-                        <button class="apply-button">Apply</button>
-                        <h4>In-person</h4>
-                        <p class="caption">
-                            Attend one of our upcoming workshops and complete a new membership form.
-                        </p>
-                    </div>
-                </div> 
             </div>
         </div>
     )
