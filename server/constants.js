40dbe3dde10868882200d1a46b6d04c7363663eb
Shiina Sugioka <shiina12@uw.edu>
2024-05-08 13:11:49 -0700

    moved testing api key to env

--- server/constants.js
+++ server/constants.js
@@ -3,3 +3,5 @@ export const PORT = process.env.PORT || 3001;
 export const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
 export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE;
 export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+export const STRIPE_TEST_API_KEY = process.env.STRIPE_TEST_API_KEY;
+export const STRIPE_TEST_API_SECRET = process.env.STRIPE_TEST_API_SECRET;
