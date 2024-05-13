318cbe74286c4c5ff191ff5be1d62cb5b0d199ef
annie-tu <annietu18@gmail.com>
2024-05-12 18:17:36 -0700

    restyle upcoming workshops desktop

--- client/src/Components/EventsPage/EventsPage.jsx
+++ client/src/Components/EventsPage/EventsPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import backgroundImg from '../../utils/images/events-background.png';
 import lockImg from '../../utils/images/lock.png';
+import calendarImg from '../../utils/images/calendar-icon.png';
+
 
 import './EventsPage.css';
 
@@ -11,21 +13,21 @@ const EVENT_INFO = [
         "speaker": "Tabitha Lieberman",
         "description": "A renowned health IT leader, Tabitha Lieberman has more than 30 years of experience powering transformational implementations, digital integrations, and deployments.",
         "flyerSource": "flyer-1.png",
-        "date": "2023-10-21"
+        "date": "2025-10-21"
     },
     {
         "title": "From Data Entry to Policy Input",
         "speaker": "Jim Condon",
         "description": "Dr. Jim Condon is an Associate Teaching Professor and Director of the Health Informatics and Health Information Management undergraduate and graduate programs at the University of Washington",
         "flyerSource": "flyer-2.png",
-        "date": "2023-11-18"
+        "date": "2025-11-18"
     },
     {
         "title": "Event 3",
         "speaker": "Speaker 3",
         "description": "description here",
         "flyerSource": "flyer-3.jpg",
-        "date": "2025-04-21"
+        "date": "2023-04-21"
     },
     {
         "title": "Spheres & Shades",
@@ -57,19 +59,30 @@ const VIDEO_INFO = [
 
 function makeUpcomingEvent(navigate, title, speaker, description, flyer, eventDate, index) {
     const flyerImg = require(`../../utils/images/${flyer}`);
-    const styleNum = index % 2 === 0 ? 1 : 2;
+    const date = new Date(eventDate);
+    date.setDate(date.getDate() + 1);
+
+    const formattedDate = date.toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric', // Day of the month
+        year: 'numeric', // Full year
+      });
+
     return (
-        <div className={"upcoming-event-section" + styleNum}>
-            <div className={"upcoming-event-image" + styleNum}>
-                <img src={flyerImg} alt="Event Flyer"/>
+        <div className={"upcoming-event-section"}>
+            <div>
+                <img className={"upcoming-event-image"} src={flyerImg} alt="Event Flyer"/>
             </div>
-            <div className={"upcoming-event-body" + styleNum}>
-                <h2 className={"upcoming-event-title" + styleNum}>
+            <div className={"upcoming-event-body"}>
+                <div className="event-date">
+                    <img src={calendarImg} alt="Calendar"/>
+                    <p>{formattedDate}</p>
+                </div>
+                <h2 className={"upcoming-event-title"}>
                     {title}<br></br><span>by {speaker}</span>
                 </h2>
-                <p className={"upcoming-event-description" + styleNum}>{description}</p>
-                <p className={"upcoming-event-date" + styleNum}>Date: {eventDate}</p>
-                <button className={"upcoming-event-button" + styleNum} onClick={() => navigate('/Registration')}>Register
+                <p className={"upcoming-event-description"}>{description}</p>
+                <button className={""} onClick={() => navigate('/Registration')}>Register
                 </button>
             </div>
         </div>
