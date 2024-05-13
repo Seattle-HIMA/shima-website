08bc063188c64eab3b25d756398d57d5aa49bbc9
Shiina Sugioka <shiina12@uw.edu>
2024-05-12 18:26:27 -0700

    modal opens on click video but not for users not logged in

--- client/src/Components/EventsPage/VideoPreviewModal.jsx
+++ client/src/Components/EventsPage/VideoPreviewModal.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
-function VideoPreviewModal({ handleModalClose }) {
+function VideoPreviewModal({ video, onClose }) {
 
   return (
-    <dialog className="modal">
-        <button className="modal-close-btn" onClick={handleModalClose}>
-          Close
-        </button>
-        <h1>Video Title</h1>
-        <p>Description:</p>
-        <p>bla bla bla</p>
-        <button>Pay to watch</button>
-    </dialog>
+      <div className="video-preview-modal">
+          <div className="video-modal-content">
+              <button className="video-model-close-button" onClick={onClose}>Close</button>
+              <h2>{video.title}</h2>
+              <div className="workshop-video-thumbnail">
+                  <img src={video.thumbnail} alt="Video Thumbnail"/>
+              </div>
+              <div className="workshop-video-description">
+                  <p>{video.description}</p>
+              </div>
+              <div className="workshop-video-link">
+                  <a href={`https://${video.link}`} target="_blank" rel="noopener noreferrer">Watch Video</a>
+              </div>
+          </div>
+      </div>
   );
 }
 
