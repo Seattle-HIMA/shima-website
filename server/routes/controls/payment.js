356c82d9c699159eaa898f9882babdec4c8ef43f
Allison-Ho <qnho@uw.edu>
2024-05-15 15:36:39 -0700

    form still ugly, still working on it

--- server/routes/controls/payment.js
+++ server/routes/controls/payment.js
@@ -17,6 +17,10 @@ const professional = await STRIPE.products.create({
     name: 'Professional membership',
 });
 
+const testVideo = await STRIPE.products.create({
+    name: 'Test workshop recording',
+});
+
 // prices for each product
 const studentPrice = await STRIPE.prices.create({
     currency: 'usd',
@@ -36,14 +40,31 @@ const profPrice = await STRIPE.prices.create({
     }
 });
 
+const VidPriceMem = await STRIPE.prices.create({
+    currency: 'usd',
+    product: testVideo.id,
+    unit_amount: 500
+});
+
+const VidPriceNonMem = await STRIPE.prices.create({
+    currency: 'usd',
+    product: testVideo.id,
+    unit_amount: 1000
+});
+
 // retrieve products' price id
 router.get('/get-product-id', (req, res) => {
-    res.json({"student_id": studentPrice.id, "prof_id": profPrice.id});
+    res.json({
+        "student_id": studentPrice.id,
+        "prof_id": profPrice.id,
+        "vid1NonMem": VidPriceNonMem.id,
+        "vid1Mem": VidPriceMem.id
+    });
 });
 
 // create a checkout session for subscription (membership)
 router.post('/create-checkout-session', async (req, res) => {
-    const {id, email, type} = req.body;
+    const {id, email, vid} = req.body;
 
     const session = await STRIPE.checkout.sessions.create({
         line_items: [
@@ -54,10 +75,10 @@ router.post('/create-checkout-session', async (req, res) => {
         ],
         mode: 'subscription',
         success_url: SUCCESSURL + `?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: REDIRECTURL,
+        cancel_url: CLIENT_ORIGIN_URL + '/Membership',
         metadata: {
             email: email,
-            product: type
+            vidName: vid
         }
     });
 
@@ -66,18 +87,22 @@ router.post('/create-checkout-session', async (req, res) => {
 
 // create a checkout session for one time payment
 router.post('/workshop-checkout-session', async (req, res) => {
-    const productId = req.body.id;
+    const {id, email, type} = req.body;
 
     const session = await STRIPE.checkout.sessions.create({
         line_items: [
             {
-                price: productId,
+                price: id,
                 quantity: 1,
             },
         ],
         mode: 'payment',
-        success_url: REDIRECTURL,
-        cancel_url: REDIRECTURL
+        success_url: SUCCESSURL,
+        cancel_url: CLIENT_ORIGIN_URL + '/Events',
+        metadata: {
+            email: email,
+            vidId: "123"
+        }
     });
 
     res.json({url: session.url});
