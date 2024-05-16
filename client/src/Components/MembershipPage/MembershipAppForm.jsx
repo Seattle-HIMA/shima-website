e9b8f2e1841f265e8c671494015400b166033f1a
Allison-Ho <qnho@uw.edu>
2024-05-16 11:07:37 -0700

    make membership app form pretty

--- client/src/Components/MembershipPage/MembershipAppForm.jsx
+++ client/src/Components/MembershipPage/MembershipAppForm.jsx
@@ -39,14 +39,14 @@ function MembershipAppForm() {
         <div className={"membership-form"}>
             <h1>Membership sign up</h1>
             <form>
-                <div className={"personal-info form-section"}>
+                <div className={"form-section"}>
                     <h2>Personal Information</h2>
                     <div className={"name-info"}>
-                        <div>
+                        <div className='left-side name-input'>
                             <label for="fname">First Name</label>
                             <input type="text" name="fname" id="fname"/>
                         </div>
-                        <div>
+                        <div className='name-input'>
                             <label for="lname">Last Name</label>
                             <input type="text" name="lname" id="lname"/>
                         </div>
@@ -62,18 +62,19 @@ function MembershipAppForm() {
                     <p>Select a membership registration type:</p>
                     <div className="membership-type-holder">
                         <div className="options-questions">
-                            <label for="professional">
-                                <p><strong>Professional</strong></p>
-                                <p className='price'>$25</p>
-                            </label>
-                            <input type="radio" id="professional" name="professional" value={profId}/>
-                        </div>
-                        <div className="options-questions">
                             <label for="student">
                                 <p><strong>Student</strong></p>
-                                <p className='price'>$10</p>
+                                <p className='price'>$10.00</p>
                             </label>
                             <input type="radio" id="student" name="student" value={studentId}/>
+                        </div>
+
+                        <div className="options-questions">
+                            <label for="professional">
+                                <p><strong>Professional</strong></p>
+                                <p className='price'>$25.00</p>
+                            </label>
+                            <input type="radio" id="professional" name="professional" value={profId}/>
                         </div>
                     </div>
                 </div>
@@ -81,11 +82,11 @@ function MembershipAppForm() {
                 <div className='form-section'>
                     <h2>Employment Status</h2>
                     <div className={"job-info"}>
-                        <div>
+                        <div className='left-side name-input'>
                             <label for="job">Current job title</label>
                             <input type="text" name="job" id="job" />
                         </div>
-                        <div>
+                        <div className='name-input'>
                             <label for="employer">Employer name</label>
                             <input type="text" name="employer" id="employer" />
                         </div>
@@ -95,51 +96,45 @@ function MembershipAppForm() {
                 <div className='form-section'>
                     <h2>Current professional credentials (select all that apply):</h2>
                     <div className='prof-credentials'>
-                        <div className='options-section'>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="rhit" name="rhit" value="RHIT"/>
-                                <label for="rhit">RHIT</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="rhia" name="rhia" value="RHIA"/>
-                                <label for="rhia">RHIA</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="ccs" name="rhit" value="CCS"/>
-                                <label for="ccs">CCS</label>
-                            </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="rhit" name="rhit" value="RHIT"/>
+                            <label for="rhit">RHIT</label>
                         </div>
-                        <div className='options-section'>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="cpc" name="cpc" value="CPC"/>
-                                <label for="cpc">CPC</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="rhit" name="rhit" value="RHIT"/>
-                                <label for="rhit">RHIT</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="cic" name="cic" value="CIC"/>
-                                <label for="cic">CIC</label>
-                            </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="rhia" name="rhia" value="RHIA"/>
+                            <label for="rhia">RHIA</label>
                         </div>
-                        <div className='options-section'>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="coc" name="coc" value="COC"/>
-                                <label for="coc">COC</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="chps" name="chps" value="CHPS"/>
-                                <label for="chps">CHPS</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="student" name="student" value="Student"/>
-                                <label for="student">Student</label>
-                            </div>
-                            <div className={"options-questions"}>
-                                <input type="checkbox" id="other" name="other" value="Other"/>
-                                <label for="other">Other</label>
-                            </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="ccs" name="rhit" value="CCS"/>
+                            <label for="ccs">CCS</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="other" name="other" value="Other"/>
+                            <label for="other">CHPS</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="cpc" name="cpc" value="CPC"/>
+                            <label for="cpc">CPC</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="rhit" name="rhit" value="RHIT"/>
+                            <label for="rhit">RHIT</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="cic" name="cic" value="CIC"/>
+                            <label for="cic">CIC</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="coc" name="coc" value="COC"/>
+                            <label for="coc">COC</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="chps" name="chps" value="CHPS"/>
+                            <label for="chps">Student</label>
+                        </div>
+                        <div className={"credentials-question"}>
+                            <input type="checkbox" id="student" name="student" value="Student"/>
+                            <label for="student">Other</label>
                         </div>
                     </div>
                 </div>
@@ -164,14 +159,14 @@ function MembershipAppForm() {
                             <label for="board">Board Member (run for an elected position)</label>
                         </div>
                         <div className={"options-questions"}>
-                            <input type="checkbox" id="committee" name="committee" value="committee"/>
-                            <label for="committee">Committee Member (involved in a specific area of SHIMA)</label>
+                            <input type="checkbox" id="speaker" name="speaker" value="speaker"/>
+                            <label for="speaker">Speaker at a future workshop</label>
                         </div>
                     </div>
                     <div className='options-section'>
                         <div className={"options-questions"}>
-                            <input type="checkbox" id="speaker" name="speaker" value="speaker"/>
-                            <label for="speaker">Speaker at a future workshop</label>
+                            <input type="checkbox" id="committee" name="committee" value="committee"/>
+                            <label for="committee">Committee Member (involved in a specific area of SHIMA)</label>
                         </div>
                         <div className={"options-questions"}>
                             <input type="checkbox" id="no" name="no" value="no"/>
@@ -180,7 +175,9 @@ function MembershipAppForm() {
                     </div>
                 </div>
 
-                <button onClick={createPaymentSession}>Continue to payment</button>
+                <div className='btn-wrapper'>
+                    <button onClick={createPaymentSession}>Continue to payment</button>
+                </div>
             </form>
         </div>
     )
